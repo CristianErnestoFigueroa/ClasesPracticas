6aac205b2f82a7a298f2a5eb9551862ace8c88b5
CristianErnestoFigueroa <79413415+CristianErnestoFigueroa@users.noreply.github.com>
2021-02-21 22:18:40 -0600

    Update

--- CristianClases/CristianPracticas/NEGOCIO/ClsLogin.cs
+++ CristianClases/CristianPracticas/NEGOCIO/ClsLogin.cs
@@ -23,30 +23,5 @@ namespace CristianPracticas.NEGOCIO
                 }
             }
             return estado;
-        }
 
-        public int accesoBidimecional(Login log)
-        {
-            string[][] usuarios;
-            usuarios = new string[2][];
-            usuarios[0] = new string[4] { "Cristian", "Ronald", "Roxana", "Erick" };
-            usuarios[1] = new string[4] { "001", "002", "003", "004" };
 
-            usuarios = usuarios.Distinct().ToArray();
-
-            int estado = 0;
-
-            for (int i = 0; i < usuarios.Length; i++)
-            {
-                for (int j = 0; j < usuarios[i].Length; j++)
-                {
-                    if (log.Usuario.Equals(usuarios[i][j]) && log.Password.Equals(usuarios[i + 1][j]))
-                    {
-                        estado = 1;
-                    }
-                }
-            }
-            return estado;
-        }
-    }
-}
